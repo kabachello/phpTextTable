43db8a213b71168ecb00bb17ab383f47460cefdb
Andrej Kabachnik <andrej_ka@gmx.de>
2017-02-28 11:30:34 +0100

    FIX issues with multibyte encoding

--- TextTable.php
+++ TextTable.php
@@ -225,7 +225,7 @@ class TextTable
         if ($this->getSeparatorRow()) {
             $result .= $this->getSeparatorCrossing();
             foreach ($this->getColumnWidths() as $val) {
-                $result .= $this->getSeparatorRow() . str_pad('', $val, $this->getSeparatorRow(), STR_PAD_RIGHT) . $this->getSeparatorRow() . $this->getSeparatorCrossing();
+                $result .= $this->getSeparatorRow() . static::mb_str_pad('', $val, $this->getSeparatorRow(), STR_PAD_RIGHT) . $this->getSeparatorRow() . $this->getSeparatorCrossing();
             }
         }
         
@@ -249,7 +249,7 @@ class TextTable
         $result .= $this->printRowLine();
         $result .= $this->getSeparatorColumn();
         foreach ($this->getColumnWidths() as $key => $val) {
-            $result .= ' ' . str_pad($header_data[$key], $val, ' ', STR_PAD_BOTH) . ' ' . $this->getSeparatorColumn();
+            $result .= ' ' . static::mb_str_pad($header_data[$key], $val, ' ', STR_PAD_BOTH) . ' ' . $this->getSeparatorColumn();
         }
         $result .= "\n";
         $result .= $this->printRowLine();
@@ -274,7 +274,7 @@ class TextTable
                         $strpad_options = STR_PAD_RIGHT;
                 }
                 $result .= " ";
-                $result .= str_pad(substr($value, ($this->getColumnWidths()[$field] * ($line - 1)), $this->getColumnWidths()[$field]), $this->getColumnWidths()[$field], ' ', $strpad_options);
+                $result .= static::mb_str_pad(mb_substr($value, ($this->getColumnWidths()[$field] * ($line - 1)), $this->getColumnWidths()[$field]), $this->getColumnWidths()[$field], ' ', $strpad_options);
                 $result .= " " . $this->getSeparatorColumn();
             }
             $result .= "\n";
@@ -425,6 +425,21 @@ class TextTable
     {
         $this->separator_column = $value;
         return $this;
+    }
+    
+    protected function mb_str_pad($str, $pad_len, $pad_str = ' ', $dir = STR_PAD_RIGHT, $encoding = NULL)
+    {
+        $encoding = $encoding === NULL ? mb_internal_encoding() : $encoding;
+        $padBefore = $dir === STR_PAD_BOTH || $dir === STR_PAD_LEFT;
+        $padAfter = $dir === STR_PAD_BOTH || $dir === STR_PAD_RIGHT;
+        $pad_len -= mb_strlen($str, $encoding);
+        $targetLen = $padBefore && $padAfter ? $pad_len / 2 : $pad_len;
+        $strToRepeatLen = mb_strlen($pad_str, $encoding);
+        $repeatTimes = ceil($targetLen / $strToRepeatLen);
+        $repeatedString = str_repeat($pad_str, max(0, $repeatTimes)); // safe if used with valid utf-8 strings
+        $before = $padBefore ? mb_substr($repeatedString, 0, floor($targetLen), $encoding) : '';
+        $after = $padAfter ? mb_substr($repeatedString, 0, ceil($targetLen), $encoding) : '';
+        return $before . $str . $after;
     }
 }
 
